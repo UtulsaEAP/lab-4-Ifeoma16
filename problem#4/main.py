f8a8e7296881b33b40a9344f1f2c33123284ea80
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-13 01:22:51 -0600

    problem 4

--- problem#4/main.py
+++ problem#4/main.py
@@ -1,23 +1,30 @@
+'''
+Name: Ifeoma Ogwu
+Lab time: Thursday, 2pm - 3:15pm
+'''
+
 def int_to_reverse_binary(num1):
     binary_val = ''
-#write your while loop here
+
     while num1 > 0:
-        #write your code
+        remainder = num1 % 2
+        binary_val += str(remainder)
+        num1 = num1 // 2
 
-    return binary_val;
+    return binary_val
 
-
-def string_reverse(input_string): 
+def string_reverse(input_string):
     reverse_input = ''
-    
-   #write your for loop here
-    
+
+    for char in input_string[::-1]:
+        reverse_input += char
+
     return reverse_input
 
 if __name__ == '__main__':
     user_input = int(input())
     
-    binary_string = str(int_to_reverse_binary(user_input))
-    binary_string = str(string_reverse(binary_string))
+    binary_string = int_to_reverse_binary(user_input)
+    reversed_binary_string = string_reverse(binary_string)
     
-    print(binary_string)
+    print(reversed_binary_string)
